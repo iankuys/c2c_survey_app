8084d62500f54b63996db91aeee5c25ad1edf8d4
delehoyb <delehoyb@uci.edu>
2023-04-12 16:36:38 -0700

    Add basic 404 page

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ def index():
     # Get hashed C2C ID from URL parameter "id"
     return render_template("index.html")
 
+@bp.app_errorhandler(404)
+def page_not_found(err):
+    return render_template("404.html"), 404
 
 ################################
 ################################
