05385d59643df6e01a0b3b48aedd51bec3cd91a5
delehoyb <delehoyb@uci.edu>
2023-04-13 14:58:08 -0700

    Support email input In case users aren't keen on copying+pasting + Use Bootstrap form field; much nicer looking & collapses better

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 import urllib.parse
 
 from flask import Blueprint, Flask, redirect, render_template, request, url_for
@@ -15,6 +16,13 @@ ERROR_MESSAGES = {"bad_key": "Invalid key."}
 # Use a Blueprint to prepend URL_PREFIX to all applicable pages
 bp = Blueprint("main_blueprint", __name__, static_folder="static", template_folder="templates")
 
+# Starting with any number of alphanumeric characters (and '.', '+', '_', '-')
+#   followed by a single '@'
+#   followed by any number of alphanumeric characters (and '.', '_', '-')
+#   followed by a single '.'
+#   ending with any number of letters
+EMAIL_REGEX = re.compile(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$")
+
 ################################
 ############ HELPERS ###########
 
@@ -22,8 +30,17 @@ bp = Blueprint("main_blueprint", __name__, static_folder="static", template_fold
 def sanitize_key(key_from_html_string: str) -> str:
     """Decodes and sanitizes user-provided 'keys' (intended to be hashed C2C IDs)."""
     result = urllib.parse.unquote_plus(key_from_html_string).strip()
-    # TODO
+    # TODO: check for suspicious characters or substrings
     return result
+
+
+def is_valid_email_address(potential_email_address: str) -> bool:
+    return bool(EMAIL_REGEX.match(potential_email_address))
+
+
+def lookup_hashed_id_by_email(email_address: str) -> str:
+    # TODO
+    return "Temporary value"
 
 
 ################################
@@ -47,12 +64,15 @@ def index():
 
 @bp.route("/check", methods=["GET", "POST"])
 def check():
-    # Endpoint that receives data from a user that manually input their key (hashed ID)
-    # to an HTML form on "/"
+    # Endpoint that receives data from a user that manually input their key (hashed ID) to an HTML form on "/"
     # Redirect to "/" with that key to check
     if "key" in request.form and len(request.form["key"]) > 0:
-        user_provided_key = urllib.parse.quote_plus(request.form["key"])
+        user_provided_key = request.form["key"].strip()
+        if is_valid_email_address(user_provided_key):
+            # print("User provided an email address")
+            user_provided_key = lookup_hashed_id_by_email(user_provided_key)
         # print("Got fallback key", user_provided_key)
+        # Don't do any intensive checking here; index() will do that
         return redirect(url_for("main_blueprint.index", key=user_provided_key), code=301)
     return redirect(url_for("main_blueprint.index"), code=301)
 
