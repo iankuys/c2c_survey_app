9e4193de461eb9b116992685bb3d412e5615396c
delehoyb <delehoyb@uci.edu>
2023-04-13 12:18:58 -0700

    Implement form for manual hashed ID input

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import urllib.parse
+
 from flask import Blueprint, Flask, redirect, render_template, request, url_for
 
 ################################
 ############ CONFIG ############
 
 URL_PREFIX = "/c2c-retention-dce"
+
+ERROR_MESSAGES = {"bad_key": "Invalid key."}
 
 ################################
 ############ STARTUP ###########
@@ -17,9 +21,11 @@ bp = Blueprint(
 ############ HELPERS ###########
 
 def sanitize_key(key_from_html_string: str) -> str:
-    result = key_from_html_string
+    """Decodes and sanitizes user-provided 'keys' (intended to be hashed C2C IDs).
+    """
+    result = urllib.parse.unquote_plus(key_from_html_string).strip()
     # TODO
-    return result.strip()
+    return result
 
 ################################
 ########### ENDPOINTS ##########
@@ -30,7 +36,12 @@ def sanitize_key(key_from_html_string: str) -> str:
 def index():
     if ("key" in request.args and len(request.args["key"]) > 0):
         hashed_id = sanitize_key(request.args["key"])
-        # Check if hashed_id is valid
+        if len(hashed_id) < 1:
+            # print("This key failed sanitization:", request.args["key"])
+            return render_template("index.html", error_message=ERROR_MESSAGES["bad_key"])
+
+        # TODO: Check if the user's key is a valid C2C hashed ID
+
         return render_template("index.html", key=hashed_id)
     return render_template("index.html")
 
@@ -39,7 +50,11 @@ def check():
     # Endpoint that receives data from a user that manually input their key (hashed ID)
     # to an HTML form on "/"
     # Redirect to "/" with that key to check
-    return redirect(url_for("main_blueprint.index"), code=302)
+    if "key" in request.form and len(request.form["key"]) > 0:
+        user_provided_key = urllib.parse.quote_plus(request.form["key"])
+        # print("Got fallback key", user_provided_key)
+        return redirect(url_for("main_blueprint.index", key=user_provided_key), code=301)
+    return redirect(url_for("main_blueprint.index"), code=301)
 
 @bp.app_errorhandler(404)
 def page_not_found(err):
