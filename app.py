b8805d3792ab4fcfca0070ef95ff70c83a8d45e8
delehoyb <delehoyb@uci.edu>
2023-04-13 11:47:16 -0700

    Receive user keys (hashed ID) From URL only for now Will create a text box that appears if no ID was found This text box will let users paste their key (hashed ID) as a fallback

--- app.py
+++ app.py
@@ -14,14 +14,32 @@ bp = Blueprint(
 )
 
 ################################
+############ HELPERS ###########
+
+def sanitize_key(key_from_html_string: str) -> str:
+    result = key_from_html_string
+    # TODO
+    return result.strip()
+
+################################
 ########### ENDPOINTS ##########
 # http://127.0.0.1:5000/c2c-retention-dce/
 
 
 @bp.route("/", methods=["GET"])
 def index():
-    # Get hashed C2C ID from URL parameter "id"
+    if ("key" in request.args and len(request.args["key"]) > 0):
+        hashed_id = sanitize_key(request.args["key"])
+        # Check if hashed_id is valid
+        return render_template("index.html", key=hashed_id)
     return render_template("index.html")
+
+@bp.route("/check", methods=["GET", "POST"])
+def check():
+    # Endpoint that receives data from a user that manually input their key (hashed ID)
+    # to an HTML form on "/"
+    # Redirect to "/" with that key to check
+    return redirect(url_for("main_blueprint.index"), code=302)
 
 @bp.app_errorhandler(404)
 def page_not_found(err):
