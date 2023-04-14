d969816df45c49420c4dd839f5248f9165a40398
delehoyb <delehoyb@uci.edu>
2023-04-14 12:41:59 -0700

    Add instrument completion column to CSV Also greatly improve documentation of config variables

--- c2c-id-hash.py
+++ c2c-id-hash.py
@@ -1,5 +1,5 @@
-# Standalone script to generate "keys" (hashed IDs) of C2C participants.
-# Outputs a CSV file that could be imported to a REDCap project for a given experiment.
+# Standalone script to generate "access keys" (hashed IDs) of C2C participants.
+# Outputs a CSV file that could be imported to a new REDCap project for a given experiment/initiative.
 
 import csv
 import hashlib
@@ -8,11 +8,31 @@ import requests
 
 import mindlib
 
+# The name of the CSV file that will be written
 CSV_FILENAME = "c2c_hashed_ids.csv"
 
+###########################
+### Options for hashing ###
+
+# String to prepend to the C2C record ID before hashing
+# Different experiments should have different prefixes to make their generated IDs different
 HASH_SALT_PREFIX = "retention_dce"
+
+# Truncate hashed IDs to this length
+# Each character increases the total amount of possible hashes by a factor of 16
+# 1 char = 16 possible hashes
+# 2 chars = 16*16 = 256 possible hashes
+# 3 chars = 16^3 = 4096 possible hashes, etc...
 HASHED_ID_LENGTH = 10
-HASHED_ID_CSV_COLUMN = "retention_dce_access_key"
+
+################################################################################
+### Options concerning the NEW REDCap project for this experiment/initiative ###
+
+# REDCap variable name that will contain the hashed ID
+REDCAP_HASHED_ID_VARIABLE = "retention_dce_access_key"
+
+# REDCap instrument that contains `REDCAP_HASHED_ID_VARIABLE` (can be found in the project's Codebook)
+REDCAP_INSTRUMENT = "basic_information"
 
 
 def export_original_c2c_ids(api_token: str, api_url: str) -> list[str]:
@@ -51,10 +71,6 @@ def hash_one_string(
     h.update(to_hash.encode())
 
     result = h.hexdigest()
-    # Each character adds to the total space of possible hashes by a factor of 16
-    # 1 char = 16 possible hashes
-    # 2 chars = 16*16 = 256 possible hashes
-    # 3 chars = 16^3 = 4096 possible hashes, etc...
     return result[:length]
 
 
@@ -91,20 +107,42 @@ def create_hashed_ids(
     return hashes_to_original_ids
 
 
-def write_csv(csv_filename: str, hashes_to_real_ids: dict[str:str], hashed_id_column_name) -> None:
+def write_csv(
+    csv_filename: str,
+    hashes_to_original_ids: dict[str:str],
+    hashed_id_column_name: str,
+    redcap_instrument: str,
+    redcap_instrument_completion: str = "1",
+) -> None:
     """Write the CSV to contain the complete mapping of C2C ID to the new hashed IDs."""
+    if redcap_instrument_completion not in ["0", "1", "2"]:
+        # "0" = "Incomplete", red light in REDCap
+        # "1" = "Unverified", yellow light in REDCap
+        # "2" = "Complete", green light in REDCap
+        raise ValueError(
+            f'REDCap instrument completion can only be "1", "2", or "3" (got "{redcap_instrument_completion}")'
+        )
+
     with open(csv_filename, "w+", newline="") as outfile:
-        fieldnames = ["record_id", "redcap_event_name", "c2c_id", hashed_id_column_name]
+        instrument_completion_variable_name = f"{redcap_instrument}_complete"
+        fieldnames = [
+            "record_id",
+            "redcap_event_name",
+            "c2c_id",
+            hashed_id_column_name,
+            instrument_completion_variable_name,
+        ]
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        for i, hashed_id in enumerate(hashes_to_real_ids, start=1):
+        for i, hashed_id in enumerate(hashes_to_original_ids, start=1):
             writer.writerow(
                 {
                     "record_id": i,
                     "redcap_event_name": "start_arm_1",
-                    "c2c_id": hashed_ids[hashed_id],
+                    "c2c_id": hashes_to_original_ids[hashed_id],
                     hashed_id_column_name: hashed_id,
+                    instrument_completion_variable_name: redcap_instrument_completion,
                 }
             )
     return
@@ -120,5 +158,5 @@ if __name__ == "__main__":
     print(f"Hashed {len(hashed_ids)}/{len(ids)} IDs ({len(hashed_ids)/len(ids)*100:.2f}%)")
 
     # Build a CSV of "record_id" and hashed IDs that can be imported to the new REDCap project for this experiment
-    write_csv(CSV_FILENAME, hashed_ids, HASHED_ID_CSV_COLUMN)
+    write_csv(CSV_FILENAME, hashed_ids, REDCAP_HASHED_ID_VARIABLE, REDCAP_INSTRUMENT)
     print(f"\nWrote '{CSV_FILENAME}'")
