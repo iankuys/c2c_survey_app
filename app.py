863b56909c63a9ca3817dddb6d8d9772c585f683
delehoyb <delehoyb@uci.edu>
2023-04-17 12:42:37 -0700

    Improve data structures for ID checks Improve code documentation

--- app.py
+++ app.py
@@ -11,20 +11,24 @@ import redcap_helpers
 
 URL_PREFIX = "/c2c-retention-dce"
 
+# The REDCap variable in this experiment's REDCap project that contains unique hashed C2C IDs
+HASHED_ID_REDCAP_VARIABLE = "retention_dce_access_key"
+
+# Configure this in tandem with c2c-id-hash.HASHED_ID_LENGTH (and the contents of the REDCap project)
 EXPECTED_HASHED_ID_LENGTH = 10
 
 ################################
 ############ STARTUP ###########
 
-# Use a Blueprint to prepend URL_PREFIX to all applicable pages
-bp = Blueprint("main_blueprint", __name__, static_folder="static", template_folder="templates")
-
-SUSPICIOUS_CHARS = [";", ":", "&", '"', "'", "`", ">", "<", "{", "}", "|", ".", "%"]
-
 ERROR_MESSAGES = {
     "bad_key": "Invalid key.",
     "bad_email": "Couldn't get access key from email address.",
 }
+
+SUSPICIOUS_CHARS = [";", ":", "&", '"', "'", "`", ">", "<", "{", "}", "|", ".", "%"]
+
+# Use a Blueprint to prepend URL_PREFIX to all applicable pages
+bp = Blueprint("main_blueprint", __name__, static_folder="static", template_folder="templates")
 
 ################################
 ############ HELPERS ###########
@@ -33,7 +37,7 @@ ERROR_MESSAGES = {
 def sanitize_key(key_from_html_string: str) -> str:
     """Decodes and sanitizes user-provided 'keys' (intended to be hashed C2C IDs)."""
     result = urllib.parse.unquote_plus(key_from_html_string).strip()
-    # Hashed IDs MUST be of a pre-specified length - any less or any more means they were hand-crafted.
+    # Hashed IDs MUST be of a pre-specified length - anything else is suspicious.
     if len(result) == EXPECTED_HASHED_ID_LENGTH and not any(
         [s in result for s in SUSPICIOUS_CHARS]
     ):
@@ -41,57 +45,53 @@ def sanitize_key(key_from_html_string: str) -> str:
     return ""
 
 
-def hashed_id_is_valid_access_key(received_hashed_id: str) -> bool:
-    print(f"Checking hashed ID {received_hashed_id}")
+def create_id_mapping(primary_key=HASHED_ID_REDCAP_VARIABLE) -> dict[str:dict]:
+    """Default behavior: returns a mapping of hashed C2C IDs to dicts containing the
+    new C2C-DCV record ID and their corresponding original C2C ID.
+    The primary key can be overridden to create mappings between other IDs, such as "c2c_id".
+    """
     all_c2c_dcv_id_records = redcap_helpers.export_redcap_report(
         app.config["C2C_DCV_API_TOKEN"],
         app.config["REDCAP_API_URL"],
         app.config["C2C_DCV_TO_ACCESS_KEYS_REPORT_ID"],
     )
-    access_keys = set(
-        r["retention_dce_access_key"]
-        for r in all_c2c_dcv_id_records
-        if "retention_dce_access_key" in r
-    )
-    return received_hashed_id in access_keys
+    result = dict()
+    for record in all_c2c_dcv_id_records:
+        if primary_key in record and "redcap_event_name" in record:
+            hashed_id = record.pop(primary_key)
+            del record["redcap_event_name"]
+            result[hashed_id] = record
+    return result
 
 
 def lookup_hashed_id_by_email(email_address: str) -> str:
+    """Attempts to obtain a participant's access key for this experiment via their C2C enrollment email.
+    Returns a string containing the access key.
+    Returns an empty string on failure:
+      - the email address isn't registered
+      - the C2C ID doesn't have an access key associated with it.
+    """
     print(f"Checking email {email_address}")
     all_email_c2c_records = redcap_helpers.export_redcap_report(
         app.config["C2CV3_API_TOKEN"],
         app.config["REDCAP_API_URL"],
         app.config["C2CV3_ALL_EMAILS_REPORT_ID"],
     )
-    emails_to_c2c_id = dict(
-        [
-            (r["start_email"], r["record_id"])
-            for r in all_email_c2c_records
-            if "start_email" in r and "record_id" in r
-        ]
-    )
+    emails_to_c2c_id = dict()
+    for record in all_email_c2c_records:
+        if "start_email" in record and "record_id" in record:
+            emails_to_c2c_id[record["start_email"]] = record["record_id"]
     if email_address not in emails_to_c2c_id:
         return ""
 
     c2c_id = emails_to_c2c_id[email_address]
     print(f"Got C2C ID from email {email_address}: {c2c_id}")
 
-    all_c2c_dcv_id_records = redcap_helpers.export_redcap_report(
-        app.config["C2C_DCV_API_TOKEN"],
-        app.config["REDCAP_API_URL"],
-        app.config["C2C_DCV_TO_ACCESS_KEYS_REPORT_ID"],
-    )
-    c2c_ids_to_access_keys = dict(
-        [
-            (r["c2c_id"], r["retention_dce_access_key"])
-            for r in all_c2c_dcv_id_records
-            if "c2c_id" in r and "retention_dce_access_key" in r
-        ]
-    )
+    c2c_ids_to_access_keys = create_id_mapping(primary_key="c2c_id")
     if c2c_id not in c2c_ids_to_access_keys:
         return ""
 
-    return c2c_ids_to_access_keys[c2c_id]
+    return c2c_ids_to_access_keys[c2c_id][HASHED_ID_REDCAP_VARIABLE]
 
 
 ################################
@@ -108,10 +108,15 @@ def index():
                 # print("This key failed sanitization:", request.args["key"])
                 return render_template("index.html", error_message=ERROR_MESSAGES["bad_key"])
 
-            if not hashed_id_is_valid_access_key(hashed_id):
+            c2c_dcv_id_records = create_id_mapping()
+
+            if hashed_id not in c2c_dcv_id_records:
                 return render_template("index.html", error_message=ERROR_MESSAGES["bad_key"])
 
-            return render_template("index.html", key=hashed_id)
+            # Success, start the experiment
+            return render_template(
+                "index.html", key=hashed_id, c2c_id=c2c_dcv_id_records[hashed_id]["c2c_id"]
+            )
         else:
             if "by_email" in request.args:
                 return render_template("index.html", error_message=ERROR_MESSAGES["bad_email"])
@@ -125,14 +130,18 @@ def check():
     if "key" in request.form and len(request.form["key"]) > 0:
         user_provided_key = request.form["key"].strip()
         if mindlib.is_valid_email_address(user_provided_key):
-            # print("User provided an email address")
+            # User typed in an email address; look up the access key
             user_provided_key = lookup_hashed_id_by_email(user_provided_key)
+            # Success, send access key to index
             return redirect(
                 url_for("main_blueprint.index", key=user_provided_key, by_email="1"), code=301
             )
+        # Not a valid email, so try interpreting this as a literal access key
+        # Don't have to do any intensive sanitizing or checking here; index() will do that
+        # Success, send access key to index
         return redirect(url_for("main_blueprint.index", key=user_provided_key), code=301)
-        # Don't do any intensive checking here; index() will do that
 
+    # Don't allow users to visit this endpoint directly
     return redirect(url_for("main_blueprint.index"), code=301)
 
 
