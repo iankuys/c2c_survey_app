2e33090883dabf5b9700ce9948a33b0b0344df79
delehoyb <delehoyb@uci.edu>
2023-04-18 17:03:09 -0700

    Add REDCap record import function

--- redcap_helpers.py
+++ redcap_helpers.py
@@ -30,3 +30,34 @@ def export_redcap_report(token: str, url: str, report_id: str | int) -> list[dic
             f"REDCap API returned an error while exporting report '{report_id}':\n{result['error']}"
         )
     return result
+
+
+def import_minimal_record(
+    token: str, url: str, record_id: str, id_variable_name: str = "record_id"
+) -> int:
+    """Makes a REDCap API call to import a single record into a project.
+    This new record is intended to only contain the record ID and no actual data.
+    """
+    request_params = {
+        "token": token,
+        "content": "record",
+        "action": "import",
+        "format": "json",
+        "type": "flat",
+        "overwriteBehavior": "normal",
+        "forceAutoNumber": "false",
+        "data": f'[{{"{id_variable_name}":"{record_id}"}}]',
+        "returnContent": "count",
+        "returnFormat": "json",
+    }
+    r = requests.post(url, data=request_params)
+    # print('>>> HTTP Status: ' + str(r.status_code))
+    result = json.loads(r.text)
+    if type(result) == dict:
+        if "error" in result:
+            raise REDCapError(
+                f"REDCap API returned an error while importing record '{record_id}':\n{result['error']}"
+            )
+        if "count" in result:
+            return result["count"]
+    return 1
