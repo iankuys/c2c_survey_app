e4daffb4458fe1675a8773ef2c3d8161e306d0ca
delehoyb <delehoyb@uci.edu>
2023-05-19 10:11:35 -0700

    Move Flask code to its own file + Rename "app.py" to "main.py"

--- flask_site.py
+++ flask_site.py
@@ -1,23 +1,12 @@
 import json
 import random
 import urllib.parse
-from typing import Any, List
 
-import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
-from fastapi.responses import RedirectResponse
 from flask import Flask, redirect, render_template, request, url_for
-from pydantic import BaseModel, BaseSettings
 
 import emails
 import mindlib
 import redcap_helpers
-
-################################
-############ CONFIG ############
-
-URL_PREFIX = "/c2c-retention-dce"
 
 # The REDCap variable in this experiment's REDCap project that contains unique hashed C2C IDs
 HASHED_ID_EXPERIMENT_REDCAP_VAR = "access_key"
@@ -28,34 +17,18 @@ HASHED_ID_C2C_REDCAP_VAR = "proj_pid_813"
 # Configure this in tandem with c2c-id-hash.HASHED_ID_LENGTH (confirm w/ the REDCap projects)
 EXPECTED_HASHED_ID_LENGTH = 12
 
-# TODO: put this in a file for easier editing
-VIDEOS = {
-    "a0": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=a0",
-    "b1": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=b1",
-    "c2": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=c2",
-    "d3": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=d3",
-    "e4": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=e4",
-    "f5": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=f5",
-    "g6": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=g6",
-    "h7": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=h7",
-    "i8": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=i8",
-    "j9": "https://player.vimeo.com/video/76979871?h=8272103f6e&id=j9",
-}
-
-
-################################
-############ STARTUP ###########
-
 BUBBLE_MESSAGES = {
     "bad_key": "Invalid key.",
     "missing_key": "Missing access key.",
     "unknown": "Unknown error.",
 }
 
+VIDEOS = mindlib.json_to_dict("videos.json")
 SUSPICIOUS_CHARS = [";", ":", "&", '"', "'", "`", ">", "<", "{", "}", "|", ".", "%"]
 
+
 flask_app = Flask(__name__)
-flask_app.config["APPLICATION_ROOT"] = URL_PREFIX
+# flask_app.config["APPLICATION_ROOT"] = URL_PREFIX
 flask_app.config.from_file("secrets.json", load=json.load)  # JSON keys must be in ALL CAPS
 
 
@@ -261,119 +234,3 @@ def videos():
 @flask_app.errorhandler(404)
 def page_not_found(err):
     return render_template("404.html"), 404
-
-
-################################
-################################
-
-app = FastAPI(openapi_url=None)
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/survey", WSGIMiddleware(flask_app))
-
-
-def json_config_settings_source(settings: BaseSettings) -> dict[str, Any]:
-    return mindlib.json_to_dict("secrets.json")
-
-
-class Settings(BaseSettings):
-    C2CV3_API_TOKEN: str
-    C2CV3_EMAILS_REPORT_ID: str
-    C2CV3_TO_ACCESS_KEYS_REPORT_ID: str
-    C2C_DCV_API_TOKEN: str
-    REDCAP_API_URL: str
-    MAIL_SMTP_SERVER_ADDR: str
-    MAIL_C2C_NOREPLY_ADDR: str
-    MAIL_C2C_NOREPLY_DISPLAY_NAME: str
-    MAIL_C2C_NOREPLY_PASS: str
-
-    class Config:
-        @classmethod
-        def customise_sources(
-            cls,
-            init_settings,
-            env_settings,
-            file_secret_settings,
-        ):
-            return (
-                init_settings,
-                json_config_settings_source,
-                env_settings,
-                file_secret_settings,
-            )
-
-
-fastapi_settings = Settings()
-
-
-class VideoIn(BaseModel):
-    """Data about a video that the client selected after finishing both videos"""
-
-    vid_id: str
-    position: int
-    pause_count: int
-    logs: List[dict]
-    user_agent: str
-
-
-class VideoOut(BaseModel):
-    """Data about a video that this server will send to the client"""
-
-    vid_id: str
-    url: str
-
-
-class VideoOutPack(BaseModel):
-    """2 VideoOuts to send to clients as JSON objects"""
-
-    videoA: VideoOut
-    videoB: VideoOut
-
-
-@app.post("/video_selected")
-async def get_video_choice(video_choice: VideoIn, key: str | None = None) -> None:
-    if key:
-        print(
-            f"User '{key}' selected this video:\n\tID '{video_choice.vid_id}'\n\tPosition '{video_choice.position}'\n\t'{video_choice.pause_count}' pauses\n\tUser Agent '{video_choice.user_agent}'\n\tLogs: {video_choice.logs}"
-        )
-        # TODO: set this screen event's "video_complete" to "2"
-        # AND set the 3rd screen's video A or B to this screen's selection - randomly? or pre-set?
-    else:
-        print("No access key detected")
-
-
-@app.get("/get_videos")
-async def send_video(key: str | None = None) -> VideoOutPack | dict:
-    if key:
-        # video_A_id, video_B_id = random.sample(list(VIDEOS.keys()), 2)
-        screens = redcap_helpers.export_video_ids(
-            fastapi_settings.C2C_DCV_API_TOKEN, fastapi_settings.REDCAP_API_URL, recordid=key
-        )
-        if len(screens) == 0:
-            # REDCap API returns an empty list if the record ID (access key) isn't in the project
-            print(f"Access key {key} not found in REDCap")
-            return {"detail": "Not Found"}
-
-        # Get the next 2 video IDs based on REDCap event completion
-        video_A_id = ""
-        video_B_id = ""
-        for screen in screens:
-            this_screen_complete = screen["video_complete"] == "2"
-            if not this_screen_complete:
-                video_A_id = screen["video_a"]
-                video_B_id = screen["video_b"]
-                break
-
-        print(f"Sending videos '{video_A_id}' and '{video_B_id}' to user '{key}'")
-        vidA = VideoOut(vid_id=video_A_id, url=VIDEOS[video_A_id])
-        vidB = VideoOut(vid_id=video_B_id, url=VIDEOS[video_B_id])
-        return VideoOutPack(videoA=vidA, videoB=vidB)
-    return {"detail": "Not Found"}
-
-
-@app.get("/")
-async def redirect_to_flask():
-    return RedirectResponse("/survey", status_code=302)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app)
