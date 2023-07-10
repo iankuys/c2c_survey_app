ee290f4377adf25089750875094bc2af8c504683
Brandon Delehoy <delehoyb@uci.edu>
2023-07-10 11:22:43 -0700

    Improve email code structure and checks

--- emails.py
+++ emails.py
@@ -11,13 +11,14 @@ from jinja2 import Template
 PORT = 465
 
 EMAIL_SUBJECT = "Your C2C survey access key"
+EMAIL_TEMPLATE_PATH = Path(".", "content", "reminder_email.html")
 
 
 def construct_message_contents(user_email_addr: str, subject: str, key: str) -> tuple[str, str]:
     """Returns a 2-tuple of strings containing content to place in an email.
     The first element is the message in plain text and the second element is the message in HTML.
     """
-    with open(Path(".", "content", "reminder_email.html")) as infile:
+    with open(EMAIL_TEMPLATE_PATH) as infile:
         # HTML
         # Do this first so we can derive the plain text FROM it
         email_jinja_template = Template(infile.read())
@@ -30,6 +31,7 @@ def construct_message_contents(user_email_addr: str, subject: str, key: str) -> 
         parser.ignore_links = True
         converted_html_message = parser.handle(html_message)
         plaintext_message = f"Subject: {subject}\n\n{converted_html_message}"
+
         return (plaintext_message, html_message)
 
 
@@ -37,17 +39,19 @@ def construct_message(
     from_addr: str, from_addr_display_name: str, to_addr: str, subject: str, key: str
 ) -> EmailMessage:
     if "@" not in from_addr:
-        raise ValueError(f"Malformed 'from' email: {from_addr}")
+        raise ValueError(f"Malformed 'from' email (missing '@'): {from_addr}")
     from_addr_parts = from_addr.split("@")
     if len(from_addr_parts) != 2:
-        raise ValueError(f"Malformed 'from' email: {from_addr}")
+        raise ValueError(f"Malformed 'from' email (incomplete address): {from_addr}")
+    if "." not in from_addr_parts[1]:
+        raise ValueError(f"Malformed 'from' email (bad domain): {from_addr}")
 
     message_html = EmailMessage()
-    message_html["Subject"] = subject
     message_html["From"] = Address(
         display_name=from_addr_display_name, username=from_addr_parts[0], domain=from_addr_parts[1]
     )
     message_html["To"] = to_addr
+    message_html["Subject"] = subject
 
     body_txt, body_html = construct_message_contents(to_addr, subject, key)
 
@@ -64,8 +68,9 @@ def send_mail(
     our_email_display_name: str,
     our_email_password: str,
 ) -> None:
-    print(f"** SENDING AN EMAIL TO '{user_email_address}' with access key '{access_key}'")
-    print(f"** FROM '{our_email_address}'@'{our_smtp_server_address}'")
+    print(
+        f"[{user_email_address}] sending access key '{access_key}' FROM '{our_email_address}'@'{our_smtp_server_address}'"
+    )
 
     # DEBUG:
     # If an "=" appears in the console printout, that's a "soft line break" and it's normal:
@@ -93,7 +98,7 @@ def send_mail(
             smtp_server.send_message(message)
     except Exception as e:
         print(
-            f"Failed to send email to {user_email_address} (via {our_smtp_server_address}:{PORT})"
+            f"[{user_email_address}] Failed to send email (via {our_smtp_server_address}:{PORT})"
         )
         raise e
         # print(repr(e))
