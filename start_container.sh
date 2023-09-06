aea59fb2fd2f690a8ece88f002019a266462bdd6
Brandon Delehoy <delehoyb@uci.edu>
2023-09-06 14:26:39 -0700

    Add "port" var to Docker container startup script

--- start_container.sh
+++ start_container.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 app="c2c-retention-dce"
+port=8080
+
 echo "Stopping ${app}"
 docker stop ${app}
 
@@ -12,6 +14,6 @@ docker build -t ${app} .
 echo "Running ${app}"
 docker run -d \
   --restart unless-stopped \
-  -p 8080:8080 \
+  -p ${port}:${port} \
   --name=${app} \
   -v $PWD:/app ${app}
